ced172c29e2ca86b51eda8cbc955df15a82eaa58
Brendan Fortuner <bfortuner@gmail.com>
2017-11-29 09:50:46 -0800

    Update activation_functions.py

--- code/activation_functions.py
+++ code/activation_functions.py
@@ -1,14 +1,25 @@
 import math
 import numpy as np
 
+### Note ###
+
+# z is weighted input
+
+
+### Functions ###
+
 def relu(z):
   return max(0, z)
-
-def relu_prime(z):
-  return 1 if z > 0 else 0
 
 def sigmoid(z):
   return 1.0 / (1 + np.exp(-z))
 
+
+
+### Derivatives ###
+
 def sigmoid_prime(z):
   return sigmoid(z) * (1-sigmoid(z))
+
+def relu_prime(z):
+  return 1 if z > 0 else 0

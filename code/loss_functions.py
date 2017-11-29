7d06f8dee27502c0dec26f13b316e0b2c200b3cf
Brendan Fortuner <bfortuner@gmail.com>
2017-11-29 10:59:44 -0800

    Update loss_functions.py

--- code/loss_functions.py
+++ code/loss_functions.py
@@ -10,45 +10,45 @@ import numpy as np
 ### Functions ###
 
 def CrossEntropy(yHat, y):
-  if yHat == 1:
-    return -log(y)
-  else:
-    return -log(1 - y)
+    if yHat == 1:
+      return -log(y)
+    else:
+      return -log(1 - y)
 
 
 def Dice(yHat, y):
-  total = np.sum(y, dim=1) + np.sum(yHat, dim=1)
-  intersection = np.sum(y * yHat, dim=1)
-  dice = (2.0 * intersection) / (total + 1e-7)
-  return np.mean(dice)
+    total = np.sum(y, dim=1) + np.sum(yHat, dim=1)
+    intersection = np.sum(y * yHat, dim=1)
+    dice = (2.0 * intersection) / (total + 1e-7)
+    return np.mean(dice)
 
 
 def Hinge(yHat, y):
-  return np.max(0, 1 - yHat * y)
+    return np.max(0, 1 - yHat * y)
 
 
 def Huber(yHat, y):
-  pass
+    pass
 
 
 def KLDivergence(yHat, y):
-  pass
+    pass
 
 
 def L1(yHat, y):
-  return np.sum(np.absolute(yHat - y))
+    return np.sum(np.absolute(yHat - y))
 
 
 def L2(yHat, y):
-  return np.sum((yHat - y)**2)
+    return np.sum((yHat - y)**2)
 
 
 def MLE(yHat, y):
-  pass
+    pass
 
 
 def MSE(yHat, y):
-  return np.sum((yHat - y)**2) / y.size
+    return np.sum((yHat - y)**2) / y.size
 
 
 ### Derivatives ###

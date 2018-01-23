1a45987dfc851165cc620ea080f6220693783c4c
Junjie YANG <albainyang@gmail.com>
2018-01-23 15:08:55 +0800

    Update Activation Function section (#11)
    * add application item for NLP
    
    * update LeakyReLU and Tanh

--- code/activation_functions.py
+++ code/activation_functions.py
@@ -8,18 +8,33 @@ import numpy as np
 
 ### Functions ###
 
+def leakyrelu(z, alpha):
+	return max(alpha * z, z)
+
 def relu(z):
   return max(0, z)
 
 def sigmoid(z):
   return 1.0 / (1 + np.exp(-z))
 
+def tanh(z):
+	return (np.exp(z) - np.exp(-z)) 
+	/ (np.exp(z) + np.exp(-z))
+
+
 
 
 ### Derivatives ###
+
+def leakyrelu_prime(z, alpha):
+	return 1 if z > 0 else alpha
 
 def sigmoid_prime(z):
   return sigmoid(z) * (1-sigmoid(z))
 
 def relu_prime(z):
   return 1 if z > 0 else 0
+
+def tanh_prime(z):
+	return 1 - np.power(tanh(z), 2)
+

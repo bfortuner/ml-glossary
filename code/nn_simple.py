eefd87d431df82f72be17a11bb811c67bde3d6a1
Brendan Fortuner <bfortuner@gmail.com>
2018-02-28 12:03:51 -0800

    Update nn_simple.py

--- code/nn_simple.py
+++ code/nn_simple.py
@@ -29,7 +29,7 @@ def backprop(x, y, Wh, Wo, lr):
     yHat = feed_forward(x, Wh, Wo)
 
     # Layer Error
-    Eo = (O - y) * relu_prime(Zo)
+    Eo = (yHat - y) * relu_prime(Zo)
     Eh = Eo * Wo * relu_prime(Zh)
 
     # Cost derivative for weights

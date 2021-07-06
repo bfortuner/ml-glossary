e4fdfd8635020268dfb025c7ac95df70f1d9675a
yelircaasi <isaac.r.riley@gmail.com>
2021-07-06 08:31:23 +0200

    fixed Sphinx deprecation in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -197,8 +197,8 @@ html_theme_options = {
 }
 
 def setup(app):
-    app.add_stylesheet('theme_overrides.css')
-
+    #app.add_stylesheet('theme_overrides.css')
+    app.add_css_file('theme_overrides.css')
 
 #html_context = {
 #    'css_files': [
